feeac5adc37f6cc70aab7fcfcb78fc841caa9eff
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2025-05-14 18:48:18 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './i18n/i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hacklabs">
       <App />
     </BrowserRouter>
   </StrictMode>
