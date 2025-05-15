a8ab94f33bd5a877e29c7399a72ced2c1590d376
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2025-05-15 14:07:20 +0200

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: './dist',
+  distDir: 'dist',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   eslint: {
     ignoreDuringBuilds: true,
@@ -12,7 +12,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  reactStrictMode: true,
 }
 
 export default nextConfig
